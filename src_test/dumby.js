591c9c6e1b54c0257c8cf253ce8eba521b6bda16
Joseph Aspelund <jaspelund@ptc.com>
2015-04-22 15:05:48 -0500

    Initial Commit

---
+++ src_test/dumby.js
@@ -0,0 +1,15 @@
+/*global describe,expect,it,beforeEach,inject,afterEach*/
+
+describe('Test Name', function() {
+
+    'use strict';
+
+    //beforeEach(module('your.module'));
+
+    beforeEach(function() {
+    });
+
+    it('test', function() {
+    });
+
+});
